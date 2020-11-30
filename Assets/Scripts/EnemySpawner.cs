de925c6349d8f86eee7fd85d21412f346f24378d
thewhitesummer <fe_granger@hotmail.com>
2020-11-30 16:22:40 -0300

    created simple UI with points and life

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,24 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySpawner : MonoBehaviour {
 
     [Range(0.1f, 10f)]
     [SerializeField] float secBetweenSpawns = 4f;
     [SerializeField] EnemyMovement enemyPrefab;
+    [SerializeField] Text pointsText;
+
+    int playerPoints = 0;
 
     void Start() {
+        pointsText.text = playerPoints.ToString();
         StartCoroutine(SpawnEnemies());
-    }
-
-    void Update() {
-        
     }
 
     IEnumerator SpawnEnemies() {
         while(true) {
             Instantiate(enemyPrefab, transform.position, Quaternion.identity, transform);
+            playerPoints++;
+            pointsText.text = playerPoints.ToString();
             yield return new WaitForSeconds(secBetweenSpawns);
         }
     }
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour {
 
     [SerializeField] int playerHealth = 10;
+    [SerializeField] Text healthText;
+
+    void Start() {
+        healthText.text = playerHealth.ToString();
+    }
 
     void OnTriggerEnter(Collider other) {
         TakeDamage(1);
+        healthText.text = playerHealth.ToString();
         PlayerLifeHandler();
     }
 
