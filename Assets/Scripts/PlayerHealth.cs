450d122ca6ea0261d4fe123846635679bcb6d6fd
thewhitesummer <fe_granger@hotmail.com>
2020-11-30 15:50:30 -0300

    created player health and damage logic

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    [SerializeField] int playerHealth = 10;
+
+    void OnTriggerEnter(Collider other) {
+        TakeDamage(1);
+        PlayerLifeHandler();
+    }
+
+    private void PlayerLifeHandler() {
+        if(playerHealth <= 0) {
+            Debug.Log("END GAME");
+        }
+    }
+
+    private void TakeDamage(int damage) {
+        playerHealth -= damage;
+    }
+}
